4a0764733ea52b94b5aba6f328a906ee6654e7fe
Jeremie Marlhens <jeremie.marlhens@dbmail.com>
2024-04-16 16:38:13 +0200

    add code to ode

---
+++ STAR_oscillator.py
@@ -0,0 +1,203 @@
+from ffl.STAR_rules import RNA_regulatory_base_module, RNA_regulatory_base_parameters
+from pysb import Model, Monomer, Parameter
+from pysb import Observable
+from pysb.simulator import BngSimulator
+import numpy as np
+
+#Oscillator, with RNA 1 inhibiting RNA 2, and RNA 2 inhibiting RNA 1:
+# 'omega' is the Avoqadro constant times the volume of the system
+# Cf Darren Wilkinson's book: https://www.taylorfrancis.com/books/mono/10.1201/b11812/stochastic-modelling-systems-biology-darren-wilkinson
+omega_val = 6 * 1e23 * np.pi/2 * 1e-15
+omega_val = 3000
+model = Model()
+Parameter('omega', omega_val)  # in L
+
+# Define the Monomer for the RNA
+Monomer("RNA_1", ['terminator', 'r', 'state'], {'state': ['init', 'full', 'partial']})
+Monomer("RNA_2", ['terminator', 'r', 'state'], {'state': ['init', 'full', 'partial']})
+
+# Define the parameters for the kinetic rates
+# RNA 2 repressing RNA 1
+parameters_RNA_1_RNA_2 = {'k_init': 1e-0,
+                'k_bind': 1e-0,
+                'k_unbind': 1e-5,
+                'k_act': 1e-2,
+                'k_act_reg': 1e-8,
+                'k_stop': 1e-4,
+                'k_stop_reg': 1e-0,
+                'k_deg': 1e-3}
+
+# RNA 1 activating RNA 2
+parameters_RNA_2_RNA_1 = {'k_init': 1e-0,
+                'k_bind': 1e+1,
+                'k_unbind': 1e-5,
+                'k_act': 1e-4,
+                'k_act_reg': 1e+1,
+                'k_stop': 1e-0,
+                'k_stop_reg': 1e-6,
+                'k_deg': 1e-3}
+
+# Define the parameters for the kinetic rates
+RNA_regulatory_base_parameters(model, RNA_2, RNA_1, parameters_RNA_2_RNA_1)
+RNA_regulatory_base_parameters(model, RNA_1, RNA_2, parameters_RNA_1_RNA_2)
+
+# Define the rules for the RNA
+RNA_regulatory_base_module(RNA_2, RNA_1, k_init_RNA_2, k_bind_RNA_2_RNA_1, k_unbind_RNA_2_RNA_1, k_act_RNA_2, k_act_reg_RNA_2_RNA_1, k_stop_RNA_2, k_stop_reg_RNA_2_RNA_1, k_deg_RNA_2)
+RNA_regulatory_base_module(RNA_1, RNA_2, k_init_RNA_1, k_bind_RNA_1_RNA_2, k_unbind_RNA_1_RNA_2, k_act_RNA_1, k_act_reg_RNA_1_RNA_2, k_stop_RNA_1, k_stop_reg_RNA_1_RNA_2, k_deg_RNA_1)
+
+# Define the observables
+Observable("obs_RNA_1_init", RNA_1(state='init'))
+Observable("obs_RNA_2_init", RNA_2(state='init'))
+Observable("obs_RNA_1_partial", RNA_1(state='partial'))
+Observable("obs_RNA_2_partial", RNA_2(state='partial'))
+Observable("obs_RNA_1_full", RNA_1(state='full'))
+Observable("obs_RNA_2_full", RNA_2(state='full'))
+
+# Define the initial conditions
+Parameter('RNA_1_0', 1e-1 * model.parameters['omega'].value)  # Initial concentration of RNA_1
+Parameter('RNA_2_0', 1e-4 * model.parameters['omega'].value) # Initial concentration of RNA_2
+model.initial(RNA_1(state='init', r=None, terminator=None), RNA_1_0)
+model.initial(RNA_2(state='init', r=None, terminator=None), RNA_2_0)
+
+# Define the simulation conditions
+t = np.linspace(0, 10, 10)
+# y = ScipyOdeSimulator(model, verbose=True).run(tspan=t).all
+
+# stochastic simulation
+t = np.linspace(0, 10000, 10000)
+y = BngSimulator(model, tspan=t, verbose=False).run(method = "nf").all
+
+# Plot the results
+import matplotlib.pyplot as plt
+plt.figure()
+plt.plot(t, y['obs_RNA_1_full'], label='RNA_1_full')
+plt.plot(t, y['obs_RNA_2_full'], label='RNA_2_full')
+plt.xlabel('Time (s)')
+plt.ylabel('Concentration (nM)')
+plt.legend(loc=0)
+plt.show()
+
+# plot RNA1 only
+plt.figure()
+plt.plot(t, y['obs_RNA_1_full'], label='RNA_1_full')
+plt.plot(t, y['obs_RNA_1_partial'], label='RNA_1_partial')
+plt.plot(t, y['obs_RNA_1_init'], label='RNA_1_init')
+plt.title('RNA_1')
+plt.xlabel('Time (s)')
+plt.ylabel('Concentration (nM)')
+plt.legend(loc=0)
+plt.show()
+
+# plot RNA2 only
+plt.figure()
+plt.plot(t, y['obs_RNA_2_full'], label='RNA_2_full')
+plt.plot(t, y['obs_RNA_2_partial'], label='RNA_2_partial')
+plt.plot(t, y['obs_RNA_2_init'], label='RNA_2_init')
+plt.title('RNA_2')
+plt.xlabel('Time (s)')
+plt.ylabel('Concentration (nM)')
+plt.legend(loc=0)
+plt.show()
+
+
+# Add RNA 3 to the system that will add a delay to the system
+# We can have RNA 3 activate RNA 1, and RNA 1 activate RNA 2, and RNA 2 inhibits RNA 3
+model = Model()
+Parameter('omega', omega_val)  # in L
+
+# Define the Monomer for the RNA
+Monomer("RNA_1", ['terminator', 'r', 'state'], {'state': ['init', 'full', 'partial']})
+Monomer("RNA_2", ['terminator', 'r', 'state'], {'state': ['init', 'full', 'partial']})
+Monomer("RNA_3", ['terminator', 'r', 'state'], {'state': ['init', 'full', 'partial']})
+
+# Define the parameters for the kinetic rates
+# RNA 2 repressing RNA 1
+parameters_RNA_1_RNA_2 = {'k_init': 1e-0,
+                'k_bind': 1e-0,
+                'k_unbind': 1e-5,
+                'k_act': 1e-2,
+                'k_act_reg': 1e-8,
+                'k_stop': 1e-4,
+                'k_stop_reg': 1e-0,
+                'k_deg': 1e-3}
+
+# RNA 1 activating RNA 2
+parameters_RNA_2_RNA_1 = {'k_init': 1e-0,
+                'k_bind': 1e+1,
+                'k_unbind': 1e-5,
+                'k_act': 1e-4,
+                'k_act_reg': 1e+3,
+                'k_stop': 1e-0,
+                'k_stop_reg': 1e-8,
+                'k_deg': 1e-3}
+
+# RNA 3 activating RNA 1
+parameters_RNA_1_RNA_3 = {'k_init': 1e-0,
+                'k_bind': 1e+1,
+                'k_unbind': 1e-5,
+                'k_act': 1e-4,
+                'k_act_reg': 1e+2,
+                'k_stop': 1e-0,
+                'k_stop_reg': 1e-6,
+                'k_deg': 1e-3}
+
+# RNA 2 inhibiting RNA 3
+parameters_RNA_3_RNA_2 = {'k_init': 1e-0,
+                'k_bind': 1e+1,
+                'k_unbind': 1e-5,
+                'k_act': 1e+2,
+                'k_act_reg': 1e-1,
+                'k_stop': 1e-4,
+                'k_stop_reg': 1e-0,
+                'k_deg': 1e-3}
+
+# Define the parameters for the kinetic rates
+RNA_regulatory_base_parameters(model, RNA_2, RNA_1, parameters_RNA_2_RNA_1)
+RNA_regulatory_base_parameters(model, RNA_3, RNA_2, parameters_RNA_3_RNA_2)
+RNA_regulatory_base_parameters(model, RNA_1, RNA_3, parameters_RNA_1_RNA_3)
+
+# Define the rules for the RNA
+RNA_regulatory_base_module(RNA_2, RNA_1, k_init_RNA_2, k_bind_RNA_2_RNA_1, k_unbind_RNA_2_RNA_1, k_act_RNA_2, k_act_reg_RNA_2_RNA_1, k_stop_RNA_2, k_stop_reg_RNA_2_RNA_1, k_deg_RNA_2)
+RNA_regulatory_base_module(RNA_3, RNA_2, k_init_RNA_3, k_bind_RNA_3_RNA_2, k_unbind_RNA_3_RNA_2, k_act_RNA_3, k_act_reg_RNA_3_RNA_2, k_stop_RNA_3, k_stop_reg_RNA_3_RNA_2, k_deg_RNA_3)
+RNA_regulatory_base_module(RNA_1, RNA_3, k_init_RNA_1, k_bind_RNA_1_RNA_3, k_unbind_RNA_1_RNA_3, k_act_RNA_1, k_act_reg_RNA_1_RNA_3, k_stop_RNA_1, k_stop_reg_RNA_1_RNA_3, k_deg_RNA_1)
+
+# Define the observables
+Observable("obs_RNA_1_init", RNA_1(state='init'))
+Observable("obs_RNA_2_init", RNA_2(state='init'))
+Observable("obs_RNA_3_init", RNA_3(state='init'))
+Observable("obs_RNA_1_partial", RNA_1(state='partial'))
+Observable("obs_RNA_2_partial", RNA_2(state='partial'))
+Observable("obs_RNA_3_partial", RNA_3(state='partial'))
+Observable("obs_RNA_1_full", RNA_1(state='full'))
+Observable("obs_RNA_2_full", RNA_2(state='full'))
+Observable("obs_RNA_3_full", RNA_3(state='full'))
+
+# Define the initial conditions
+Parameter('RNA_1_0', 1e-1 * model.parameters['omega'].value)  # Initial concentration of RNA_1
+Parameter('RNA_2_0', 1e-4 * model.parameters['omega'].value) # Initial concentration of RNA_2
+Parameter('RNA_3_0', 1e-4 * model.parameters['omega'].value) # Initial concentration of RNA_3
+model.initial(RNA_1(state='init', r=None, terminator=None), RNA_1_0)
+model.initial(RNA_2(state='init', r=None, terminator=None), RNA_2_0)
+model.initial(RNA_3(state='init', r=None, terminator=None), RNA_3_0)
+
+# Define the simulation conditions
+t = np.linspace(0, 10, 10)
+# y = ScipyOdeSimulator(model, verbose=True).run(tspan=t).all
+
+# stochastic simulation
+t = np.linspace(0, 20000, 10000)
+y = BngSimulator(model, tspan=t, verbose=False).run(method = "nf").all
+
+# Plot the results
+import matplotlib.pyplot as plt
+plt.figure()
+plt.plot(t, y['obs_RNA_1_full'], label='RNA_1_full')
+plt.plot(t, y['obs_RNA_2_full'], label='RNA_2_full')
+plt.plot(t, y['obs_RNA_3_full'], label='RNA_3_full')
+# plt.plot(t, y['obs_RNA_1_partial'], label='RNA_1_partial')
+# plt.plot(t, y['obs_RNA_2_partial'], label='RNA_2_partial')
+# plt.plot(t, y['obs_RNA_3_partial'], label='RNA_3_partial')
+plt.xlabel('Time (s)')
+plt.ylabel('Concentration (nM)')
+plt.legend(loc=0)
+plt.show()
---
+++ utils/print_odes.py
@@ -0,0 +1,126 @@
+import re
+import sympy as sp
+import numpy as np
+
+
+# Function to substitute expressions in an ODE string
+def substitute_expressions(ode_string, expression_dict):
+    for expr_name, expr_definition in expression_dict.items():
+        # Replace each expression placeholder with its definition
+        # This uses a regular expression to find whole word matches only, to avoid partial replacements
+        pattern = r'\b' + re.escape(expr_name) + r'\b'
+        ode_string = re.sub(pattern, f'({expr_definition})', ode_string)
+    return ode_string
+
+def find_ODEs_from_Pysb_model(model):
+    #
+    equations = []
+    for i, eq in enumerate(model._odes):
+        equations += ["d" + model.observables[i].name + f"/dt = {eq}"]
+
+    #
+    name_mapping = {}
+    for i, species in enumerate(model.species):
+        name_mapping[f'__s{i}'] = model.observables[i].name
+    expression_dict = {expr.name: str(expr.expand_expr()) for expr in model.expressions}
+
+    # Substitute expressions in the ODEs
+    equations = [substitute_expressions(eq, expression_dict) for eq in equations]
+    equations = [substitute_expressions(eq, name_mapping) for eq in equations]
+
+    return equations
+
+def simpy_equations_from_string(equations):
+    # Extract observables as variables
+    variables = sp.symbols([obs.name for obs in model.observables])
+
+    # Extract parameters and their values
+    parameters = {param.name: param.value for param in model.parameters[::-1]}
+
+    # Dynamically create sympy symbols for parameters
+    param_symbols = sp.symbols(list(parameters.keys()))
+
+    all_symbols_dict = {**{str(var): var for var in variables}, **{str(param): param for param in param_symbols}}
+
+    # Assuming 'equations' is your list of ODE strings
+    equations_sym = []
+    for eq_str in equations:
+        pass
+        # Split each equation string at '=',
+        # taking the part after '=' as the expression to be solved for steady state
+        ode_rhs = eq_str.split('=')[1]
+
+        # Substitute parameter names with their values in the expression
+        for param_name, param_value in parameters.items():
+            ode_rhs = ode_rhs.replace(param_name, str(param_value))
+
+        # Convert to a symbolic expression
+        expr = sp.sympify(ode_rhs, locals=all_symbols_dict)
+        equations_sym.append(expr - 0)  # Subtract 0 to imply '= 0' for steady state
+
+    return equations_sym, variables
+
+def simpy_equations_from_Pysb_model(model):
+    equations = find_ODEs_from_Pysb_model(model)
+    equations_sym, variables = simpy_equations_from_string(equations)
+    return equations_sym, variables
+
+def convert_to_latex(equations):
+    # Convert the ODEs into LaTeX format with specified modifications
+    formatted_equations = []
+    for eq in equations:
+        # Replace ** with ^ for exponentiation in LaTeX
+        eq = eq.replace('**', '^')
+
+        # Add subscript to variable names that have an underscore and subsequent characters
+        eq = re.sub(r'(\w+)_(\w+)', r'\1_{\2}', eq)
+
+        # remove "*1"
+        eq = re.sub(r'\*(1)', '', eq)
+
+        # Simplify multiplication by -1 by targeting the entire expression preceding it
+        # Look for expressions enclosed in parentheses followed by *(-1) and prepend a negative sign
+        eq = re.sub(r'\(([^)]+)\)\*\(\-1\)', r'-\(\1\)', eq)
+        # Address any instances not enclosed by parentheses, though this case should be rare
+        eq = re.sub(r'(\w+)\*\(\-1\)', r'-\1', eq)
+
+        # Convert derivatives and general fractions to LaTeX fraction format
+        # Handle derivatives first
+        eq = re.sub(r'(d\w+_{\w+})/(d\w+)', r'\\frac{\1}{\2}', eq)
+        # Handle more complex expressions
+        eq = re.sub(r'(\([^)]+\))\/(\([^)]+\))', r'\\frac{\1}{\2}', eq)
+        # Handle simple variable or constant fractions
+        eq = re.sub(r'(\b\w+|\b\w+_{\w+})\/(\b\w+|\b\w+_{\w+})', r'\\frac{\1}{\2}', eq)
+
+        # Align the equation using &=
+        eq = eq.replace('=', '&=')
+
+        formatted_equations.append(eq)
+
+    # Combine all equations into one, separated by \\
+    combined_equations = ' \\\\\n'.join(formatted_equations)
+    return f'\\begin{{align*}}\n{combined_equations}\n\\end{{align*}}'
+
+def write_to_file(latex_code, filename="odes.tex"):
+    # Write the LaTeX-formatted equations to a file
+    with open(filename, 'w') as file:
+        file.write('\\documentclass{article}\n\\usepackage{amsmath}\n\\begin{document}\n')
+        file.write(latex_code + '\n')
+        file.write('\\end{document}')
+
+
+if __name__ == '__main__':
+    from pysb.examples.robertson import model
+    # simulate the model
+    tspan = np.linspace(0, 300, 3000)
+    from pysb.simulator import ScipyOdeSimulator
+    sim = ScipyOdeSimulator(model, tspan)
+    simres = sim.run()
+
+
+    equations = find_ODEs_from_Pysb_model(model)
+    print(equations)
+
+    # Convert and write to file
+    latex_equations = convert_to_latex(equations)
+    write_to_file(latex_equations)
