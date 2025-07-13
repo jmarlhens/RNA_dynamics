4065fd07d77a60964eb7f082bb403a0e80fa278f
Jeremie Marlhens <jeremie.marlhens@dbmail.com>
2025-07-13 22:08:00 +0200

    updating data for new circuits

--- data/circuits/circuit_configs.py
+++ data/circuits/circuit_configs.py
@@ -12,6 +12,12 @@ DATA_FILES = {
     "cffl_type_1": "../../data/data_parameter_estimation/c1_ffl_and.csv",
     "star_antistar_1": "../../data/data_parameter_estimation/star_antistar.csv",
     "constitutive sfGFP": "../../data/data_parameter_estimation/positive_control_sfGFP.csv",
+    "cffl_12": "../../data/data_parameter_estimation/C-1,2-FFL.csv",
+    "c-1fffl_or": "../../data/data_parameter_estimation/SI C-1FFL OR-based.csv",
+    "inhibited_cascade": "../../data/data_parameter_estimation/antiTrigger with Cascade.csv",
+    "or_gate_c1ffl": "../../data/data_parameter_estimation/SI C-1FFL OR-based.csv",
+    "inhibited_incoherent_cascade": "../../data/data_parameter_estimation/antiTrigger with Toehold.csv",
+    "iffl_1": "../../data/data_parameter_estimation/I1-FFL.csv",
 }
 
 # Circuit condition parameters
@@ -203,118 +209,242 @@ CIRCUIT_CONDITIONS = {
     },
     "cffl_12": {
         # STAR6 fixed at 15nM, STAR1 titrated
-        "STAR1_0nM_STAR6_15nM": {
-            "k_Star1_concentration": 0,
+        "STAR1 15 nM": {
+            "k_Star1_concentration": 15,
             "k_Star6_concentration": 15,
-            "k_Sense1_aSTAR6_concentration": 5,
+            "k_Sense1_aStar6_concentration": 5,
             "k_Sense6_Trigger3_concentration": 5,
             "k_Sense6_Toehold3_GFP_concentration": 3,
         },
-        "STAR1_3nM_STAR6_15nM": {
-            "k_Star1_concentration": 3,
-            "k_Star6_concentration": 15,
-            "k_Sense1_aSTAR6_concentration": 5,
-            "k_Sense6_Trigger3_concentration": 5,
-            "k_Sense6_Toehold3_GFP_concentration": 3,
-        },
-        "STAR1_5nM_STAR6_15nM": {
-            "k_Star1_concentration": 5,
-            "k_Star6_concentration": 15,
-            "k_Sense1_aSTAR6_concentration": 5,
-            "k_Sense6_Trigger3_concentration": 5,
-            "k_Sense6_Toehold3_GFP_concentration": 3,
-        },
-        "STAR1_10nM_STAR6_15nM": {
+        "STAR1 10 nM": {
             "k_Star1_concentration": 10,
             "k_Star6_concentration": 15,
-            "k_Sense1_aSTAR6_concentration": 5,
+            "k_Sense1_aStar6_concentration": 5,
             "k_Sense6_Trigger3_concentration": 5,
             "k_Sense6_Toehold3_GFP_concentration": 3,
         },
-        "STAR1_15nM_STAR6_15nM": {
-            "k_Star1_concentration": 15,
+        "STAR1 5 nM": {
+            "k_Star1_concentration": 5,
             "k_Star6_concentration": 15,
-            "k_Sense1_aSTAR6_concentration": 5,
+            "k_Sense1_aStar6_concentration": 5,
             "k_Sense6_Trigger3_concentration": 5,
             "k_Sense6_Toehold3_GFP_concentration": 3,
         },
-        # STAR1 fixed at 15nM, STAR6 titrated
-        "STAR1_15nM_STAR6_0nM": {
-            "k_Star1_concentration": 15,
-            "k_Star6_concentration": 0,
-            "k_Sense1_aSTAR6_concentration": 5,
+        "STAR1 3 nM": {
+            "k_Star1_concentration": 3,
+            "k_Star6_concentration": 15,
+            "k_Sense1_aStar6_concentration": 5,
             "k_Sense6_Trigger3_concentration": 5,
             "k_Sense6_Toehold3_GFP_concentration": 3,
         },
-        "STAR1_15nM_STAR6_3nM": {
-            "k_Star1_concentration": 15,
-            "k_Star6_concentration": 3,
-            "k_Sense1_aSTAR6_concentration": 5,
+        "Without STAR1": {
+            "k_Star1_concentration": 0,
+            "k_Star6_concentration": 15,
+            "k_Sense1_aStar6_concentration": 5,
             "k_Sense6_Trigger3_concentration": 5,
             "k_Sense6_Toehold3_GFP_concentration": 3,
         },
-        "STAR1_15nM_STAR6_5nM": {
-            "k_Star1_concentration": 15,
-            "k_Star6_concentration": 5,
-            "k_Sense1_aSTAR6_concentration": 5,
+        # STAR1 fixed at 3nM, STAR6 titrated
+        "STAR6 15 nM": {
+            "k_Star1_concentration": 3,
+            "k_Star6_concentration": 15,
+            "k_Sense1_aStar6_concentration": 5,
             "k_Sense6_Trigger3_concentration": 5,
             "k_Sense6_Toehold3_GFP_concentration": 3,
         },
-        "STAR1_15nM_STAR6_10nM": {
-            "k_Star1_concentration": 15,
+        "STAR6 10 nM": {
+            "k_Star1_concentration": 3,
             "k_Star6_concentration": 10,
-            "k_Sense1_aSTAR6_concentration": 5,
+            "k_Sense1_aStar6_concentration": 5,
+            "k_Sense6_Trigger3_concentration": 5,
+            "k_Sense6_Toehold3_GFP_concentration": 3,
+        },
+        "STAR6 5 nM": {
+            "k_Star1_concentration": 3,
+            "k_Star6_concentration": 5,
+            "k_Sense1_aStar6_concentration": 5,
+            "k_Sense6_Trigger3_concentration": 5,
+            "k_Sense6_Toehold3_GFP_concentration": 3,
+        },
+        "STAR6 3 nM": {
+            "k_Star1_concentration": 3,
+            "k_Star6_concentration": 3,
+            "k_Sense1_aStar6_concentration": 5,
+            "k_Sense6_Trigger3_concentration": 5,
+            "k_Sense6_Toehold3_GFP_concentration": 3,
+        },
+        "Without STAR6": {
+            "k_Star1_concentration": 3,
+            "k_Star6_concentration": 0,
+            "k_Sense1_aStar6_concentration": 5,
             "k_Sense6_Trigger3_concentration": 5,
             "k_Sense6_Toehold3_GFP_concentration": 3,
         },
     },
+    "inhibited_cascade": {
+        # plasmids are star6_plasmid, toehold3_gfp_plasmid, atrigger3_plasmid
+        # The reporter plasmid concentration was fixed at
+        # 3 nM, the Sense–Trigger plasmid at 5 nM, and the STAR plasmid at 15 nM, while the
+        # concentration of the antiTrigger plasmid was titrated from 0 to 15 nM (0, 3, 5, 10, and 15 nM).
+        "0 nM": {
+            "k_Star6_concentration": 15,
+            "k_Sense6_Trigger3_concentration": 5,
+            "k_Toehold3_GFP_concentration": 3,
+            "k_aTrigger3_concentration": 0,
+        },
+        "3 nM": {
+            "k_Star6_concentration": 15,
+            "k_Sense6_Trigger3_concentration": 5,
+            "k_Toehold3_GFP_concentration": 3,
+            "k_aTrigger3_concentration": 3,
+        },
+        "5 nM": {
+            "k_Star6_concentration": 15,
+            "k_Sense6_Trigger3_concentration": 5,
+            "k_Toehold3_GFP_concentration": 3,
+            "k_aTrigger3_concentration": 5,
+        },
+        "10 nM": {
+            "k_Star6_concentration": 15,
+            "k_Sense6_Trigger3_concentration": 5,
+            "k_Toehold3_GFP_concentration": 3,
+            "k_aTrigger3_concentration": 10,
+        },
+        "15 nM": {
+            "k_Star6_concentration": 15,
+            "k_Sense6_Trigger3_concentration": 5,
+            "k_Toehold3_GFP_concentration": 3,
+            "k_aTrigger3_concentration": 15,
+        },
+    },
     "iffl_1": {
         # STAR-Trigger fixed at 15nM, Sense-antiTrigger titrated
-        "STARTrigger_15nM_antiTrigger_0nM": {
+        "Sense-aTrigger 0 nM": {
             "k_Star6_Trigger3_concentration": 15,
             "k_Sense6_aTrigger3_concentration": 0,
             "k_Toehold3_GFP_concentration": 3,
         },
-        "STARTrigger_15nM_antiTrigger_3nM": {
+        "Sense-aTrigger 3 nM": {
             "k_Star6_Trigger3_concentration": 15,
             "k_Sense6_aTrigger3_concentration": 3,
             "k_Toehold3_GFP_concentration": 3,
         },
-        "STARTrigger_15nM_antiTrigger_5nM": {
+        "Sense-aTrigger 5 nM": {
             "k_Star6_Trigger3_concentration": 15,
             "k_Sense6_aTrigger3_concentration": 5,
             "k_Toehold3_GFP_concentration": 3,
         },
-        "STARTrigger_15nM_antiTrigger_10nM": {
+        "Sense-aTrigger 10 nM": {
             "k_Star6_Trigger3_concentration": 15,
             "k_Sense6_aTrigger3_concentration": 10,
             "k_Toehold3_GFP_concentration": 3,
         },
-        "STARTrigger_15nM_antiTrigger_15nM": {
+        "Sense-aTrigger 15 nM": {
             "k_Star6_Trigger3_concentration": 15,
             "k_Sense6_aTrigger3_concentration": 15,
             "k_Toehold3_GFP_concentration": 3,
         },
-        # Sense-antiTrigger fixed at 5nM, STAR-Trigger titrated
-        "STARTrigger_0nM_antiTrigger_5nM": {
+        "STAR-Trigger 0 nM": {
             "k_Star6_Trigger3_concentration": 0,
             "k_Sense6_aTrigger3_concentration": 5,
             "k_Toehold3_GFP_concentration": 3,
         },
-        "STARTrigger_3nM_antiTrigger_5nM": {
+        "STAR-Trigger 3 nM": {
             "k_Star6_Trigger3_concentration": 3,
             "k_Sense6_aTrigger3_concentration": 5,
             "k_Toehold3_GFP_concentration": 3,
         },
-        "STARTrigger_5nM_antiTrigger_5nM": {
+        "STAR-Trigger 5 nM": {
             "k_Star6_Trigger3_concentration": 5,
             "k_Sense6_aTrigger3_concentration": 5,
             "k_Toehold3_GFP_concentration": 3,
         },
-        "STARTrigger_10nM_antiTrigger_5nM": {
+        "STAR-Trigger 10 nM": {
             "k_Star6_Trigger3_concentration": 10,
             "k_Sense6_aTrigger3_concentration": 5,
             "k_Toehold3_GFP_concentration": 3,
+        },
+        "STAR-Trigger 15 nM": {
+            "k_Star6_Trigger3_concentration": 15,
+            "k_Sense6_aTrigger3_concentration": 5,
+            "k_Toehold3_GFP_concentration": 3,
+        },
+    },
+    "inhibited_incoherent_cascade": {
+        # The reporter plasmid concentration was fixed at 3 nM, the Sense–Trigger plasmid at
+        # 5 nM, the Trigger plasmid at 15 nM, and the Trigger plasmid at 3 nM, while the concentra-
+        # tion of the STAR plasmid was titrated from 0 to 15 nM.
+        # plasmids are star6_plasmid, toehold3_gfp_plasmid, trigger3_plasmid, sense6_atrigger3_plasmid
+        "0 nM": {
+            "k_Star6_concentration": 0,
+            "k_Sense6_aTrigger3_concentration": 5,
+            "k_Toehold3_GFP_concentration": 3,
+            "k_Trigger3_concentration": 15,
+        },
+        "5 nM": {
+            "k_Star6_concentration": 5,
+            "k_Sense6_aTrigger3_concentration": 5,
+            "k_Toehold3_GFP_concentration": 3,
+            "k_Trigger3_concentration": 15,
+        },
+        "10 nM": {
+            "k_Star6_concentration": 10,
+            "k_Sense6_aTrigger3_concentration": 5,
+            "k_Toehold3_GFP_concentration": 3,
+            "k_Trigger3_concentration": 15,
+        },
+        "15 nM": {
+            "k_Star6_concentration": 15,
+            "k_Sense6_aTrigger3_concentration": 5,
+            "k_Toehold3_GFP_concentration": 3,
+            "k_Trigger3_concentration": 15,
+        },
+    },
+    "or_gate_c1ffl": {
+        # The concentration of each reporter
+        # plasmid was fixed at 1.5 nM, the intermediate (Sense–Trigger) plasmid at 5 nM, and the
+        # STAR plasmid concentration was titrated from 0 to 15 nM.
+        "0 nM": {
+            "k_Star6_concentration": 0,
+            "k_Sense6_Trigger3_concentration": 5,
+            "k_Sense6_GFP_concentration": 1.5,
+            "k_Toehold3_GFP_concentration": 1.5,
+        },
+        "3 nM": {
+            "k_Star6_concentration": 3,
+            "k_Sense6_Trigger3_concentration": 5,
+            "k_Sense6_GFP_concentration": 1.5,
+            "k_Toehold3_GFP_concentration": 1.5,
+        },
+        "5 nM": {
+            "k_Star6_concentration": 5,
+            "k_Sense6_Trigger3_concentration": 5,
+            "k_Sense6_GFP_concentration": 1.5,
+            "k_Toehold3_GFP_concentration": 1.5,
+        },
+        "7 nM": {
+            "k_Star6_concentration": 7,
+            "k_Sense6_Trigger3_concentration": 5,
+            "k_Sense6_GFP_concentration": 1.5,
+            "k_Toehold3_GFP_concentration": 1.5,
+        },
+        "10 nM": {
+            "k_Star6_concentration": 10,
+            "k_Sense6_Trigger3_concentration": 5,
+            "k_Sense6_GFP_concentration": 1.5,
+            "k_Toehold3_GFP_concentration": 1.5,
+        },
+        "12 nM": {
+            "k_Star6_concentration": 12,
+            "k_Sense6_Trigger3_concentration": 5,
+            "k_Sense6_GFP_concentration": 1.5,
+            "k_Toehold3_GFP_concentration": 1.5,
+        },
+        "15 nM": {
+            "k_Star6_concentration": 15,
+            "k_Sense6_Trigger3_concentration": 5,
+            "k_Sense6_GFP_concentration": 1.5,
+            "k_Toehold3_GFP_concentration": 1.5,
         },
     },
 }
